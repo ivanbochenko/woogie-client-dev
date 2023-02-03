44b5086c83267348a06c32964501e2443d9934de
ivan <bochenkoivan@gmail.com>
2023-02-03 21:21:20 +0200

    m

--- constants/Keys.js
+++ constants/Keys.js
@@ -1,4 +1,3 @@
-export const dsn = 'https://a333b78b80fb47e88b026a6c4830e60b@o4504108695683072.ingest.sentry.io/4504108698828800'
 export const clientId = '1222094605234870'
 export const redirectUri = 'https://auth.expo.io/@bochenko/woogie-client-dev'
 export const baseURL = 'https://woogie-server.herokuapp.com'
